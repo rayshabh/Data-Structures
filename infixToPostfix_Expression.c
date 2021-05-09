40024d439ad11750ec05306151e0590cb3012186
rayshabh-codes <khargareeshav@gmail.com>
2021-05-10 00:17:48 +0530

    minor changes

--- infixToPostfix_Expression.c
+++ infixToPostfix_Expression.c
@@ -24,9 +24,19 @@ struct STACK //Stack Declaration
     int MAX_SIZE;
     int TOP;
     char *operatorStack;
-} * ptr;
+};
 
-bool isEmpty() //checks if Stack is empty or not
+struct STACK *createSTACK(int size)
+{
+    struct STACK *stack = (struct STACK *)malloc(sizeof(struct STACK));
+    stack->MAX_SIZE = size;
+    stack->TOP = -1;
+    stack->operatorStack = (char *)malloc(stack->MAX_SIZE * sizeof(char));
+
+    return stack;
+}
+
+bool isEmpty(struct STACK *ptr) //checks if Stack is empty or not
 {
     if (ptr->TOP == -1)
         return true;
@@ -34,7 +44,7 @@ bool isEmpty() //checks if Stack is empty or not
         return false;
 }
 
-bool isFull() //checks if Stack is empty or not
+bool isFull(struct STACK *ptr) //checks if Stack is empty or not
 {
     if (ptr->TOP == ptr->MAX_SIZE)
         return true;
@@ -42,12 +52,12 @@ bool isFull() //checks if Stack is empty or not
         return false;
 }
 
-void push(char operator) //push operation
+void push(struct STACK *ptr, char operator) //push operation
 {
     ptr->operatorStack[++ptr->TOP] = operator;
 }
 
-char pop() //pop operation
+char pop(struct STACK *ptr) //pop operation
 {
     return ptr->operatorStack[(ptr->TOP--)];
 }
@@ -82,44 +92,44 @@ int checkPrecedence(char operator) //function to check the precedence of the ope
 
 void infixToPostfix(char *expression, int MAX_SIZE) //converts infix expression to postfix expression
 {
-    ptr = (struct STACK *)malloc(sizeof(struct STACK));
-    ptr->MAX_SIZE = MAX_SIZE;
-    ptr->TOP = -1;
-    ptr->operatorStack = (char *)malloc(ptr->MAX_SIZE * sizeof(char *));
+    struct STACK *stack = createSTACK(SIZE);
 
     int i = 0, j = 0;
     bool flagParenthesis = false, flagOperand = false; //flag variables to check if the expression is valid or not
 
     while (expression[i] != '\0')
     {
-
         if (isOperand(expression[i])) //checks if scanned element is operand
         {
-            expression[j++] = expression[i]; //if the scanned element is an operand, append it to the expression
-            flagOperand = true;              //set true, if the first scanned element is an operand
+            expression[j++] = expression[i]; //if the scanned element is an operand, append it to the
+            flagOperand = true;              //expression. Set true, if the first scanned element
+                                             //is an operand
         }
 
         else if (expression[i] == '(') //checks if scanned element is '('
         {
-            push(expression[i]);    //if yes, push it to the stack
-            flagParenthesis = true; //set true, if the first scanned element is '('
+            push(stack, expression[i]); //if yes, push it to the stack
+            flagParenthesis = true;     //set true, if the first scanned element is '('
         }
 
         else if (expression[i] == ')') //if ')' parenthesis is encountered
         {
-            if (isEmpty()) //checks if stack is empty or not
-            {              //if the expression starts with ')', then the expression is not valid.
+            if (isEmpty(stack)) //checks if stack is empty or not
+            {                   //if the expression starts with ')', then the expression is not valid.
                 printf("\nThe parenthesis in the expression is unbalanced!");
                 return;
             }
-            else if (!isEmpty() && flagParenthesis == false) //checks if the stack contains '(' or not
-            {                                                //if not, then the expression is not valid
+
+            else if (!isEmpty(stack) && flagParenthesis == false) //checks if the stack contains '('
+                                                                  //or not
+            {
                 printf("\nThe parenthesis in the expression is unbalanced!");
                 return;
             }
-            while (ptr->operatorStack[ptr->TOP] != '(') //scans the stack till '(' and pops every operator out of the stack
-                expression[j++] = pop();                //and append it to the expression
-            ptr->TOP--;
+
+            while (stack->operatorStack[stack->TOP] != '(') //scans the stack till '(' and pops every operator out of the stack and append it to the expression
+                expression[j++] = pop(stack);
+            stack->TOP--;
             flagParenthesis = false; //set false, if popping completes.
         }
 
@@ -131,32 +141,33 @@ void infixToPostfix(char *expression, int MAX_SIZE) //converts infix expression 
                 printf("\nInvalid Expression!");
                 return;
             }
-            if (!isEmpty() && checkPrecedence(expression[i]) <= checkPrecedence(ptr->operatorStack[ptr->TOP])) //checks the scanned
-            {                                                                                                  //operator with the
-                while (checkPrecedence(expression[i]) <= checkPrecedence(ptr->operatorStack[ptr->TOP]))        //operator in Stack
-                    expression[j++] = pop();                                                                   //TOP and compares
-                push(expression[i]);                                                                           //the precedence
+
+            if (!isEmpty(stack) && checkPrecedence(expression[i]) <= checkPrecedence(stack->operatorStack[stack->TOP])) //checks the scanned operator with the operator in Stack TOP and compares the precedence
+            {
+                while (checkPrecedence(expression[i]) <= checkPrecedence(stack->operatorStack[stack->TOP]))
+                    expression[j++] = pop(stack);
+                push(stack, expression[i]);
             }
+
             else
-                push(expression[i]); //push the scanned operator into stack if the precedence is higher
+                push(stack, expression[i]); //push the scanned operator into stack if the precedence is higher
         }
         i++;
     }
 
-    while (!isEmpty()) //popp out operators from the stack
+    while (!isEmpty(stack)) //popp out operators from the stack
     {
-        char temp = pop();
+        char temp = pop(stack);
         if (temp == '(') //check if '(' is present in the stack, if yes, the expression is unbalanced
         {
             printf("\nThe parenthesis in the expression is unbalanced!");
             return;
         }
         expression[j++] = temp; //append the popped operator to the expression
-        expression[j] = '\0';
     }
-
+    expression[j] = '\0';
     printf("\nThe postifix expression: %s", expression);
-    free(ptr);
+    free(stack);
 }
 int main()
 {
